d26205bd06442f0fbe2f62dfee0a6189c980f671
“Vishnu <vishnukayahan84@gmail.com>
2023-07-23 11:11:08 +0530

    Introduction typeScript variable

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-let age:number=30;
-console.log('my age is :'+age);
+//variable =>>>>
+let age:number=25;
+let studentName:string='saman kumar';
+let isActive:boolean=true;
+let myObject:null=null;
+let undefinedData:undefined=undefined;
+let animals:Array<string>=['','',''];
+let vehicles:string[]=['','',''];
+let student:{name:string,age:number}={name:'ranil',age:20};
+
+//--------------
+interface Animal{
+    name:string;
+    bread:string;
+    age:number
+}
+let dog:Animal={name:'TOGO',bread:'Husky',age:1};
