e0b3843220de366c52f3afce0abfe1370109f732
“Vishnu <vishnukayahan84@gmail.com>
2023-07-23 13:37:05 +0530

    Introduction typeScript method

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ let numbers:number[]=[10,20,30,40];
 console.log(numbers[0]);//10
 
 //-----------------
-let string:Array<string>=['','','','']
+let strings:Array<string>=['','','','']
 
 //-----------------
+strings.push('10');//add
+strings.pop();//rid the last element
+
+///=========
