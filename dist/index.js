a4dba6d6c9737e1565f631fc6fc74edfcfc2ec69
“Vishnu <vishnukayahan84@gmail.com>
2023-07-23 13:29:54 +0530

    Introduction typeScript typePreference

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,3 @@
 "use strict";
-let age = 25;
-let studentName = 'saman kumar';
-let isActive = true;
-let myObject = null;
-let undefinedData = undefined;
-let animals = ['', '', ''];
-let vehicles = ['', '', ''];
-let student = { name: 'ranil', age: 20 };
-let dog = { name: 'TOGO', bread: 'Husky', age: 1 };
-console.log(dog.bread);
-function getAgeAfterThreeY(age, name) {
-    if (name) {
-        console.log(`Hello ${name}`);
-    }
-    else {
-        console.log(`Hello age ${age}`);
-    }
-    return age + 3;
-}
-let myAge = getAgeAfterThreeY(10);
-let myNewAge = getAgeAfterThreeY(25, 'mahinda');
-let rate = 1.5;
-rate = 2.5;
+let myName = 'saman';
+console.log(typeof myName);
