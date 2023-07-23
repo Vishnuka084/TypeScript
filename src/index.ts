06862a4234ae4b07202be515b26d8909c0277abd
“Vishnu <vishnukayahan84@gmail.com>
2023-07-23 13:42:45 +0530

    Introduction typeScript for loop

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,10 @@ let strings:Array<string>=['','','','']
 strings.push('10');//add
 strings.pop();//rid the last element
 
-///=========
+///=========>tuple
+let student:[string,number]=['saman',20];
+
+// ---------
+for (let i=0;i<numbers.length;i++){
+    console.log(numbers[i]);
+}
